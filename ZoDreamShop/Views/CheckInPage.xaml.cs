4c955e792f65828cd1db28f4b7114d0253abd161
ZoDream <648383079@qq.com>
2019-09-03 22:44:10 +0800

    修改

--- ZoDreamShop/Views/CheckInPage.xaml.cs
+++ ZoDreamShop/Views/CheckInPage.xaml.cs
@@ -12,8 +12,6 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-using ZoDream.Shop.Controls;
-using ZoDream.Shop.ViewModels;
 
 // https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
@@ -22,28 +20,19 @@ namespace ZoDream.Shop.Views
     /// <summary>
     /// 可用于自身或导航至 Frame 内部的空白页。
     /// </summary>
-    public sealed partial class MemberPage : Page
+    public sealed partial class CheckInPage : Page
     {
-        public MemberPage()
+        public CheckInPage()
         {
             this.InitializeComponent();
         }
 
-        public MemberViewModel ViewModel { get; private set; } = new MemberViewModel();
-
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             base.OnNavigatedTo(e);
-            ViewModel.User = App.ViewModel.User;
-        }
-
-        private void IconMenuItem_Tapped(object sender, TappedRoutedEventArgs e)
-        {
-            var label = (sender as IconMenuItem).Label;
-            if (label == "签到")
-            {
-                Frame.Navigate(typeof(CheckInPage));
-            }
+            picker.MinDate = DateTime.Now.AddDays(1 - DateTime.Now.Day).Date;
+            picker.MaxDate = DateTime.Now.AddDays(1 - DateTime.Now.Day).Date.AddMonths(1).AddSeconds(-1);
+            //picker.SelectedDates.Add(DateTime.Now);
         }
     }
 }
